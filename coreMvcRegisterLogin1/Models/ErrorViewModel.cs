354adaafe0ef52030206a5db3f746775ea50fd3a
Roman <57426099+roman-net@users.noreply.github.com>
2020-06-17 22:41:41 -0700

    auth1

---
+++ coreMvcRegisterLogin1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace coreMvcRegisterLogin1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
